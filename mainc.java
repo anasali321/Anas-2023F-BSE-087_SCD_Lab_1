7f940360efdc0d209918691e1b923913eb914f38
anasali321 <154501187+anasali321@users.noreply.github.com>
2025-10-27 15:45:59 +0500

    Add files via upload
    commit

---
+++ mainc.java
@@ -0,0 +1,40 @@
+package amna;
+
+import amna.user.Receotionist;
+import amna.user.billing;
+import amna.user.doctor;
+import amna.user.patient;
+
+public class mainc {
+	public static void main(String args[]){
+		user u =new user();
+		u.choice();
+		
+		Receotionist r =new Receotionist();
+		r.id=1;
+		r.name="amna";
+	
+		doctor d =new doctor();
+		d.id=2;
+		d.name="hassan";
+		
+	
+		patient p =new patient();
+		p.id=9;
+		p.name="muzammil";
+		
+		billing b = new billing(1,p.id,p.name,500);
+		
+		r. GiveAppointment();
+		d.checkpatient();
+		r.generatebill();
+		b.show();
+		p.paybill();
+			
+	}
+
+	
+}
+
+
+
